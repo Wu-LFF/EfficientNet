501b4a3e7e5d2970a7bc72e1cbbea1d78dd3013e
imxiaowu <2643915795@qq.com>
2022-03-07 15:09:07 +0800

    Initial commit

---
+++ dataloader.py
@@ -0,0 +1,72 @@
+#-*-coding:utf-8-*-
+import torch
+from torchvision import transforms, datasets
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import os
+import random
+seed = None
+
+#torch.cuda.manual_seed(seed)
+#random.seed(seed)
+
+root = r"./"
+
+# 自定义图片图片读取方式，可以自行增加resize、数据增强等操作
+def MyLoader(path):
+    return Image.open(path).convert('RGB')
+
+# 预处理=>将各种预处理组合在一起
+input_size = 224 #224, 240, 260
+data_tf_train = transforms.Compose(
+                [
+                 transforms.Resize(input_size),
+                 #transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+                 transforms.RandomHorizontalFlip(),
+                 transforms.ToTensor(),
+                 transforms.Normalize((0.1307,), (0.3081,))
+                 ])
+
+data_tf_test = transforms.Compose(
+                [
+                 transforms.Resize(input_size),
+                 transforms.ToTensor(),
+                 transforms.Normalize((0.1307,), (0.3081,))
+                 ])
+                 
+class MyDataset(Dataset):
+    # 构造函数设置默认参数
+    def __init__(self,txt,transform=None, target_transform=None, loader=MyLoader):
+        with open(txt, 'r') as fh:
+            imgs = []
+            for line in fh:
+                line = line.strip('\n')  # 移除字符串首尾的换行符
+                line = line.rstrip()  # 删除末尾空
+                words = line.split(' ')  # 以空格为分隔符 将字符串分成
+                #print(words)
+                imgs.append((words[0], int(words[1])))  # imgs中包含有图像路径和标签
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        fn, label = self.imgs[index]
+        # 调用定义的loader方法
+        img = self.loader(fn)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, label
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+train_data = MyDataset(txt=os.path.join(root,'train_data.txt'), transform=data_tf_train) #data_tf_train
+val_data = MyDataset(txt=os.path.join(root,'val_data.txt'), transform=data_tf_test)
+
+# train_data 和test_data包含多有的训练与测试数据，调用DataLoader批量加载
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=16)
+
+print('加载成功！')
---
+++ train.py
@@ -0,0 +1,118 @@
+import os
+import math
+import argparse
+import torch
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+import torch.optim.lr_scheduler as lr_scheduler
+
+root = r"./"
+from model import efficientnet_b0 as create_model
+from dataloader import train_loader, val_loader
+from utils import read_split_data, train_one_epoch, evaluate
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    print(args)
+    print('Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/')
+    tb_writer = SummaryWriter()
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+
+    img_size = {"B0": 224,
+                "B1": 240,
+                "B2": 260,
+                "B3": 300,
+                "B4": 380,
+                "B5": 456,
+                "B6": 528,
+                "B7": 600}
+    num_model = "B0"
+
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    # 如果存在预训练权重则载入
+    model = create_model(num_classes=args.num_classes).to(device)
+    if args.weights != "":
+        if os.path.exists(args.weights):
+            weights_dict = torch.load(args.weights, map_location=device)
+            load_weights_dict = {k: v for k, v in weights_dict.items()
+                                 if model.state_dict()[k].numel() == v.numel()}
+            print(model.load_state_dict(load_weights_dict, strict=False))
+        else:
+            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+
+    # 是否冻结权重
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除最后一个卷积层和全连接层外，其他权重全部冻结
+            if ("features.top" not in name) and ("classifier" not in name):
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=1E-4)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+
+    # 用于比较成功率
+    best_acc = 0.0
+
+    for epoch in range(args.epochs):
+        # train
+        mean_loss = train_one_epoch(model=model,
+                                    optimizer=optimizer,
+                                    data_loader=train_loader,
+                                    device=device,
+                                    epoch=epoch)
+
+        scheduler.step()
+
+        # validate
+        acc = evaluate(model=model,
+                       data_loader=val_loader,
+                       device=device)
+        print("[epoch {}] accuracy: {}".format(epoch, round(acc, 3)))
+        tags = ["loss", "accuracy", "learning_rate"]
+        tb_writer.add_scalar(tags[0], mean_loss, epoch)
+        tb_writer.add_scalar(tags[1], acc, epoch)
+        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
+
+        torch.save(model.state_dict(), "./weights/model-{}.pth".format(epoch))
+
+        if best_acc < acc:
+            best_acc = acc
+
+            torch.save(model.state_dict(), "./weights/model-best.pth")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=2)
+    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--lr', type=float, default=0.0003)
+    parser.add_argument('--lrf', type=float, default=0.0003)
+
+    # 数据集所在根目录
+    # http://download.tensorflow.org/example_images/flower_photos.tgz
+    parser.add_argument('--data-path', type=str,
+                        default="./")
+
+    # download model weights
+    # 链接: https://pan.baidu.com/s/1ouX0UmjCsmSx3ZrqXbowjw  密码: 090i
+    parser.add_argument('--weights', type=str, default='./efficientnetb0.pth',
+                        help='initial weights path')
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
