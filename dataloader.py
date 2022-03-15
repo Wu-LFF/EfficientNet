12c62fc62e54ec76c128392b9b2a6eacec226e5f
Wu-LFF <87291208+Wu-LFF@users.noreply.github.com>
2022-03-15 10:34:35 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -14,7 +14,7 @@ root = r"./"
 
 # 自定义图片图片读取方式，可以自行增加resize、数据增强等操作
 def MyLoader(path):
-    return Image.open(path).convert('RGB')
+    return Image.open(path)
 
 # 预处理=>将各种预处理组合在一起
 input_size = 224 #224, 240, 260
@@ -42,7 +42,7 @@ class MyDataset(Dataset):
             for line in fh:
                 line = line.strip('\n')  # 移除字符串首尾的换行符
                 line = line.rstrip()  # 删除末尾空
-                words = line.split(' ')  # 以空格为分隔符 将字符串分成
+                words = line.split('\t')  # 以空格为分隔符 将字符串分成
                 #print(words)
                 imgs.append((words[0], int(words[1])))  # imgs中包含有图像路径和标签
         self.imgs = imgs
@@ -62,11 +62,11 @@ class MyDataset(Dataset):
         return len(self.imgs)
 
 
-train_data = MyDataset(txt=os.path.join(root,'train_data.txt'), transform=data_tf_train) #data_tf_train
-val_data = MyDataset(txt=os.path.join(root,'val_data.txt'), transform=data_tf_test)
+train_data = MyDataset(txt=os.path.join(root,'train1.txt'), transform=data_tf_train) #data_tf_train
+val_data = MyDataset(txt=os.path.join(root,'test1.txt'), transform=data_tf_test)
 
 # train_data 和test_data包含多有的训练与测试数据，调用DataLoader批量加载
-train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
-val_loader = DataLoader(dataset=val_data, batch_size=16)
+train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=32)
 
 print('加载成功！')
